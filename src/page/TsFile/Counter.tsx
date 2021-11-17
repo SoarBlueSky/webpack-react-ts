c524ed0a6ad6bf0a1cbcefabe3ce6de04c594a1f
lyp000119 <2319460338@qq.com>
2021-11-17 13:54:34 +0800

    feat: add twonum algorithm

--- src/page/TsFile/Counter.tsx
+++ src/page/TsFile/Counter.tsx
@@ -1,11 +1,31 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
 import { RootState } from '../../store'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
 export function Counter() {
   const count = useSelector((state: RootState) => state.counter.value); //获取redux里面的值
   const dispatch = useDispatch() //使用dispatch去修改值
-  const [i,setI] = useState(1);
+
+  function twoSum(nums: number[], target: number){ //两数之和
+    let map = {};
+    for(let key in nums){
+        let l = target - nums[key];
+        if(map[l] !== undefined){
+            return [map[l],Number(key)];
+        }
+        map[nums[key]] = Number(key);
+    }
+    // 4 {} '1'
+    // 5 {6: 0} '2'
+    // 7 {5: 1, 6: 0} '3'
+    // 9 {3: 2, 5: 1, 6: 0} '4'
+    // 3 {1: 3, 3: 2, 5: 1, 6: 0} '5'
+  };
+
+  useEffect(() =>{
+    let arr = twoSum([6,5,3,1,7,4,3,9,8],10);
+    console.log(arr,'arr');
+  },[])
 
   return (
     <div>
